77e5069e038d360cd9e552b1941696a16e577196
Osmair Coelho <osmair.coelho@gmail.com>
2024-05-28 15:46:26 -0300

    API-17: [validation rules] quesiton required

--- tests/Feature/Question/StoreTest.php
+++ tests/Feature/Question/StoreTest.php
@@ -36,3 +36,16 @@ test('after creating a new question, I need to make sure that it creates on _dra
        'question' => 'Loren ipsum jeremias?'
     ]);
 });
+
+describe('validation rules', function (){
+    test('question::required', function (){
+       $user = User::factory()->create();
+
+       Sanctum::actingAs($user);
+
+       postJson(route('questions.store', []))
+           ->assertJsonValidationErrors([
+              'question' => 'required'
+           ]);
+    });
+});

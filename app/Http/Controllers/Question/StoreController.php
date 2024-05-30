8c4af73da1ce9e0d7751caaffe293e4126d48b3a
Osmair Coelho <osmair.coelho@gmail.com>
2024-05-30 14:50:01 -0300

    API-17: refatoring to user relationships instead of infere the user_id on the creation

--- app/Http/Controllers/Question/StoreController.php
+++ app/Http/Controllers/Question/StoreController.php
@@ -5,19 +5,17 @@ namespace App\Http\Controllers\Question;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreRequest;
 use App\Http\Resources\QuestionResource;
-use App\Models\Question;
-use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request)
     {
-        $question = Question::create([
-           'user_id' => auth()->user()->id,
-            'status' => 'draft',
-            'question' => $request->question
-        ]);
+        $question = user()
+            ->questions()
+            ->create([
+                 'status' => 'draft',
+                 'question' => $request->question
+            ]);
 
         return QuestionResource::make($question);
     }

3a8dd76468b35e9b6d6e9de3cd08dc0ad680e95c
Osmair Coelho <osmair.coelho@gmail.com>
2024-05-31 14:50:30 -0300

    API-18: validation::question should be required

--- tests/Feature/Question/EditTest.php
+++ tests/Feature/Question/EditTest.php
@@ -9,9 +9,7 @@ it('should be able to update a question', function () {
 
     $user = User::factory()->create();
 
-    $question = Question::factory()->create([
-        'user_id'=>$user->id
-    ]);
+    $question = Question::factory()->create(['user_id'=>$user->id]);
 
     Sanctum::actingAs($user);
 
@@ -25,5 +23,69 @@ it('should be able to update a question', function () {
         'question' => 'Updating a question?',
     ]);
 
+});
+
+describe('validation rules', function () {
+    test('question::required', function () {
+        $user = User::factory()->create();
+        $question = Question::factory()->create(['user_id' => $user->id]);
+
+        Sanctum::actingAs($user);
+
+        putJson(route('questions.update', $question), [
+            'question' => ''
+        ])->assertJsonValidationErrors([
+            'question' => 'required'
+        ]);
+    });
+
+    test('question::ending with question mark', function () {
+        $user = User::factory()->create();
+
+        Sanctum::actingAs($user);
+
+        postJson(route('questions.store', [
+            'question' => 'Question without a question mark',
+        ]))
+
+            ->assertJsonValidationErrors([
+                'question' => 'The question should end with question mark (?).',
+            ]);
+    });
+
+    test('question::min characters should be 10', function () {
+        $user = User::factory()->create();
+
+        Sanctum::actingAs($user);
+
+        postJson(route('questions.store', [
+            'question' => 'Question?',
+        ]))
+
+            ->assertJsonValidationErrors([
+                'question' => 'least 10 characters',
+            ]);
+    });
+
+    test('question::should be unique', function () {
+
+        $user = User::factory()->create();
+
+        Question::factory()->create([
+            'question' => 'Lorem ipsum jeremias?',
+            'status'   => 'draft',
+            'user_id'  => $user->id,
+        ]);
+
+        Sanctum::actingAs($user);
+
+        postJson(route('questions.store', [
+            'question' => 'Lorem ipsum jeremias?',
+        ]))
+
+            ->assertJsonValidationErrors([
+                'question' => 'already been taken',
+            ]);
+    });
 });
 

e92796ff8bba51a55ae40733ddb8810d6eb3e03d
Osmair Coelho <osmair.coelho@gmail.com>
2024-06-17 12:15:23 -0300

    API-20: validations: name

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    public function __invoke()
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
     {
-        User::create(request()->all());
+        $data = $request->validate([
+            'name'  => ['required', 'min:3', 'max:255'],
+            'email' => ['required', 'min:3', 'max:255', 'email'],
+        ]);
+
+        User::create($data);
     }
 }
--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\User;
+
 use Illuminate\Support\Facades\Hash;
 
 use function Pest\Laravel\{assertDatabaseHas, postJson};
@@ -9,18 +10,51 @@ use function PHPUnit\Framework\assertTrue;
 it('should be able to register in the application', function () {
     postJson(route('register'), [
         'name'     => 'John Doe',
-        'email'    => 'joe@joe.com',
+        'email'    => 'joe@doe.com',
         'password' => 'password',
-    ])->assertSessionHasNoErrors();
+    ])->assertOk();
 
     assertDatabaseHas('users', [
         'name'  => 'John Doe',
         'email' => 'joe@doe.com',
     ]);
 
-    $joeDoe = User::WhereEmail('joe@joe.com')->first();
+    $joeDoe = User::whereEmail('joe@doe.com')->first();
 
     assertTrue(
         Hash::check('password', $joeDoe->password)
     );
+});
+
+describe('validations', function () {
+
+    test('name', function ($rule, $value, $meta = []) {
+        postJson(route('register'), ['name' => $value])
+            ->assertJsonValidationErrors([
+                'name' => __(
+                    'validation.' . $rule,
+                    array_merge(['attribute' => 'name'], $meta)
+                ),
+            ]);
+    })->with([
+        'required' => ['required', ''],
+        'min:3'    => ['min', 'AB', ['min' => 3]],
+        'max:255'  => ['max', str_repeat('*', 256), ['max' => 255]],
+    ]);
+
+    /*test('name', function ($rule, $value, $meta = []) {
+        postJson(route('register'), ['name' => $value])
+            ->assertJsonValidationErrors([
+                'name' => __(
+                    'validation.' . $rule,
+                    array_merge(['attribute' => 'email'], $meta)
+                ),
+            ]);
+    })->with([
+        'required' => ['required', ''],
+        'min:3'    => ['min', 'AB', ['min' => 3]],
+        'max:255'  => ['max', str_repeat('*', 256), ['max' => 255]],
+        'email'    => ['email' => ['not-email']],
+    ]);*/
+
 });
